382be227a8c6b3f1a06f5791099b3e63f6ec2475
gaming <gaming@t2-campus.eu>
2023-02-13 15:40:30 +0100

    added second court, changed origin point of pins

--- Assets/PointCounter.cs
+++ Assets/PointCounter.cs
@@ -9,7 +9,7 @@ public class PointCounter : MonoBehaviour
    
     [SerializeField] private  TMP_Text scoreText;
     private int score;
-    [SerializeField] private GameObject ball;
+    [SerializeField] private static List<Gameobject> targets = new List<Gameobject>();
     
 
     // Start is called before the first frame update
@@ -18,24 +18,11 @@ public class PointCounter : MonoBehaviour
         scoreText.text = "Score: 0";
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "target")
-        {
-            score += 1;
-            scoreText.text = "Score: " + score;
-            Destroy(other);
-        }
-        if (other.tag == "ball")
-        {
-            Destroy(other);
-           GameObject ball2 =  Instantiate(ball);
-           ball2.transform.position = new Vector3(0.501f,-0.851f,0.482f);
-        }
-    }
+    
 
     void Update()
     {
+        
         scoreText.text = "Score: " + score;
     }
     
