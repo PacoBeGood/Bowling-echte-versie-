e11dee06f46edd90745a6d8b6204659ef3560518
Yentl Sorgi <Yentlsorgi@gmail.com>
2023-02-17 10:01:03 +0100

    added simple point system

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+
+    public bool hasFallen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasFallen = false;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (gameObject.transform.position.y < 0.149f) hasFallen = true;
+    }
+}
---
+++ Assets/Scripts/PointCounter.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PointCounter : MonoBehaviour
+{
+
+    [SerializeField] private TMP_Text scoreText;
+    private int score = 0;
+    [SerializeField] List<Pin> pins;
+    [SerializeField] List<GameObject> pin;
+    [SerializeField] private Button resetButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: 0";
+        resetButton.onClick.AddListener(Reset);
+    }
+
+
+
+    void Update()
+    {
+        foreach (Pin p in pins)
+        {
+            if (p.transform.position.y < 0.15f)
+            {
+                if (p.hasFallen == false) score += 1;
+
+                if (p.hasFallen == true)
+                {
+                    foreach (GameObject b in pin)
+                    {
+                        Destroy(b);
+                    }
+                }
+
+
+            }
+        }
+        scoreText.text = "Score: " + score;
+    }
+
+    void Reset()
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
